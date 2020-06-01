8a228bab3fe63c6be9dfaddbf336b3ed830c2e8d
waihongtan <waihong_tan@mymail.sutd.edu.sg>
2020-06-02 07:23:17 +0800

    added documentation

--- src/receive_scan.py
+++ src/receive_scan.py
@@ -12,27 +12,14 @@ import tf2_geometry_msgs
 from tf.transformations import *
 import time
 
-
+#create receive scan class that receives mqtt scan emssage and publish to ros
 class ReceiveScan():
     def __init__(self):
-        #rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, self.pose_callback)
+        #init class variables
         self.pose = None
         pass
-    
-    # def conv_msg_pose(self,msg):
-    #     pub = rospy.Publisher('%s/pose'%(subscribed_robot), LaserScan, queue_size=10)
-    #     msg_trans = msg["translation"]
-    #     msg_rot = msg["rotation"]
-    #     msg_pose = PoseWithCovarianceStamped()
-    #     msg_pose.header.frame_id = "%s/base_scan"%(subscribed_robot)
-    #     msg_pose.pose.pose.position.x = msg_trans["x"]
-    #     msg_pose.pose.pose.position.y = msg_trans["y"]
-    #     msg_pose.pose.pose.position.z = msg_trans["z"]
-    #     msg_pose.pose.pose.orientation.x = msg_rot["x"]
-    #     msg_pose.pose.pose.orientation.y = msg_rot["y"]
-    #     msg_pose.pose.pose.orientation.z = msg_rot["z"]
-    #     msg_pose.pose.pose.orientation.w = msg_rot["w"]
 
+    #transform laser data to map frame with tf2
     def transform_scan(self,msg):
         print(type(msg["translation"]["x"]))
         br = tf2_ros.TransformBroadcaster()     
@@ -42,18 +29,7 @@ class ReceiveScan():
         t.child_frame_id = "%s/base_scan"%(subscribed_robot)
         msg_trans = msg["translation"]
         msg_rot = msg["rotation"]
-        # q1 = [0,0,0,0]
-        # q2 = [0,0,0,0]
-        # q1[0]= msg_rot["x"]
-        # q1[1] = msg_rot["y"]
-        # q1[2] = msg_rot["z"]
-        # q1[3] = msg_rot["w"]
-        # q2[0] = self.pose.pose.orientation.x 
-        # q2[1] = self.pose.pose.orientation.y
-        # q2[2] = self.pose.pose.orientation.z 
-        # q2[3] = self.pose.pose.orientation.w
-        # q_new = quaternion_multiply(q1,q2)
-        # print(q1[2],q2[2],q_new[2])
+        #converting transform parameters with lidar poses transmitted through mqtt
         t.transform.translation.x = msg_trans["x"] 
         t.transform.translation.y = msg_trans["y"]
         t.transform.translation.z = msg_trans["z"]
@@ -63,13 +39,13 @@ class ReceiveScan():
         t.transform.rotation.w = msg_rot["w"]
         br.sendTransform(t)
 
+    #publish scan data to robot_name/scan topic
     def publish_scan(self,msg):
         pub = rospy.Publisher('%s/scan'%(subscribed_robot), LaserScan, queue_size=1000)
         #rate = ros
         # py.Rate(10)
         #while not rospy.is_shutdown():
         scan_msg = LaserScan()
-        #scan_msg = message_converter.convert_dictionary_to_ros_message("sensor_msgs/LaserScan", msg)
         scan_msg.header.frame_id = "%s/base_scan"%(subscribed_robot)
         scan_msg.angle_increment= msg["angle_increment"]
         scan_msg.angle_max = msg["angle_max"]
@@ -81,17 +57,12 @@ class ReceiveScan():
         scan_msg.intensities = msg["intensities"]
         pub.publish(scan_msg)
         rospy.logdebug("ROS published to {}/scan".format(subscribed_robot))
-        #rate.sleep()
-    
-    # def pose_callback(self,data):
-    #     self.pose = data.pose
-    #     rospy.logdebug("ROS received from {}".format("amcl_pose"))
 
-
+#create MQTTClient class that subcribes to the lidar data from other robots
 class MQTTClient(ReceiveScan):
     
     def __init__(self,client):
-        ReceiveScan.__init__(self)
+        ReceiveScan.__init__(self) #init ReceiveScan Class through class inheritance
         client.on_publish= self.on_publish
         client.on_subscribe= self.on_subscribe
         client.on_connect= self.on_connect
@@ -99,8 +70,8 @@ class MQTTClient(ReceiveScan):
         client.on_message = self.on_message
         self.count = 0
         self.now = time.time()
-        self.frequency = 0
-        client.username_pw_set(username,password)
+        self.frequency = 0 #flag that keep track of the message receive frequency for mqtt
+        client.username_pw_set(username,password) #MQTT Broker password and username
         client.reconnect_delay_set(min_delay=1, max_delay=6000)
         client.connect(broker, port, keepalive)
         client.loop_start()
@@ -124,6 +95,7 @@ class MQTTClient(ReceiveScan):
             print("Unexpected disconnection") 
             pass
 
+    #callback function for every message received from MQTT
     def on_message(self,client, userdata, msg): 
         print("Received message: on topic " + str(msg.topic) + " " + "with QoS " + str(msg.qos))
         self.count+= 1
@@ -133,6 +105,7 @@ class MQTTClient(ReceiveScan):
             self.count = 0
             self.now = time.time()
         m_in = msg.payload.decode("utf-8","ignore")
+        #convert json type data to dictionary python data structure
         m_json = json.loads(m_in)
         m_scan_json = json.loads(m_json["scan"])
         m_transform_json = json.loads(m_json["transform"])
@@ -144,6 +117,7 @@ class MQTTClient(ReceiveScan):
 
 if __name__ == "__main__":
     rospy.init_node('receive_scan')
+    ##### get parameters for parameter server #######
     robot_name =  rospy.get_param("/robot_name", 'robot2')
     subscribed_robot = rospy.get_param("/subscribe_robot",'robot1')
     broker=rospy.get_param("/mqtt_broker", 'localhost')
@@ -151,6 +125,7 @@ if __name__ == "__main__":
     username=rospy.get_param("/username", 'robot')
     password=rospy.get_param("/password", '1234')
     keepalive=60
+    ##### init mqtt client #######
     client = mqtt.Client("%s/subscriber"%(robot_name), clean_session= False, userdata=None)
     mqtt_start = MQTTClient(client)
     rospy.spin()
